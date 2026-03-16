7637bd201176691ad4af892f6f78d1ef555398f1
eagalyaelangovan-prog <eagalyaelangovan@gmail.com>
2026-03-16 14:05:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 st.title("🤖 AI Study Assistant Chatbot")
 
